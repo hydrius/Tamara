6cc81f252f8aaec2f5c61f9e8f23cd9cb2fa032f
elder-of-ozone <a.fordham@iinet.net.au>
2017-03-13 20:39:17 +0800

    added randomness to movement

--- Tamara/plugins/movement.py
+++ Tamara/plugins/movement.py
@@ -1,5 +1,6 @@
 import serial
-
+import datetime
+import random
 
 class Movement():
     def __init__(self, Tamara):
@@ -12,13 +13,18 @@ class Movement():
         self.ser = serial.Serial(address)
 
     def run(self):
+        self.now = datetime.datetime.now().time()
 
         # serial in
         serin = self.ser.readline().decode("utf-8")
 
         print(serin)
         if "movement" in serin:
-            self.Tamara.say("movement")
+            diff = datetime.datetime.combine(datetime.date.min, self.now) - datetime.datetime.combine(datetime.date.min, self.prev)
+            if diff > 300:
+                if random.random() < 0.05:
+                    self.Tamara.say("Hello!")
+            self.prev = self.now
 
 
 
