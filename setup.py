2ae78b768ddd676ea2e43bac23247181174aa115
elder-of-ozone <a.fordham@iinet.net.au>
2017-03-13 20:39:56 +0800

    added randomness to movement

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Tamara',
-      version='0.1.6',
+      version='0.1.7',
       #packages=['Tamara'],
       #package_dir = {'': 'sensors'},
       packages=find_packages(),
